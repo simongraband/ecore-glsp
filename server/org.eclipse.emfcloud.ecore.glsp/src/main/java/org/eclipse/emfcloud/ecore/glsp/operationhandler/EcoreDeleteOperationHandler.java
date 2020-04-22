20138ebc87c44d49a8e8040d657f371165c80eac
Simon Graband <simon.graband@tum.de>
2020-04-22 14:28:52 +0200

    Add Deletion for Inheritance edges

--- server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/operationhandler/EcoreDeleteOperationHandler.java
+++ server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/operationhandler/EcoreDeleteOperationHandler.java
@@ -10,30 +10,47 @@
  ********************************************************************************/
 package org.eclipse.emfcloud.ecore.glsp.operationhandler;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emfcloud.ecore.glsp.model.EcoreModelState;
 import org.eclipse.emfcloud.ecore.glsp.util.EcoreEdgeUtil;
+import org.eclipse.emfcloud.ecore.glsp.util.EcoreConfig.Types;
 import org.eclipse.glsp.api.model.GraphicalModelState;
+import org.eclipse.glsp.graph.GEdge;
 import org.eclipse.glsp.graph.GModelIndex;
 import org.eclipse.glsp.server.operationhandler.DeleteOperationHandler;
 
 public class EcoreDeleteOperationHandler extends DeleteOperationHandler {
 	@Override
 	protected boolean delete(String elementId, GModelIndex index, GraphicalModelState graphicalModelState) {
-		
+
 		super.delete(elementId, index, graphicalModelState);
 		EcoreModelState modelState = EcoreModelState.getModelState(graphicalModelState);
-		
-		modelState.getIndex().getSemantic(elementId).ifPresent(element -> {
-			if(element instanceof EReference && ((EReference) element).getEOpposite() != null) {
+
+		modelState.getIndex().getSemantic(elementId).ifPresentOrElse(element -> {
+			if (element instanceof EReference && ((EReference) element).getEOpposite() != null) {
 				EcoreUtil.delete(((EReference) element).getEOpposite());
-				modelState.getIndex().getBidirectionalReferences().remove(EcoreEdgeUtil.getStringId((EReference)element));
+				modelState.getIndex().getBidirectionalReferences()
+						.remove(EcoreEdgeUtil.getStringId((EReference) element));
 			}
+		}, () -> {
+			//just for Debugging
+			GModelIndex modelIndex = modelState.getIndex();
+
+			modelState.getIndex().findElementByClass(elementId, GEdge.class).ifPresent(edge -> {
+				if (edge.getType().equals(Types.INHERITANCE)) {
+					modelState.getIndex().getSemantic(edge.getSourceId(), EClass.class).ifPresent(source -> {
+						EClass target = modelState.getIndex().getSemantic(edge.getTargetId(), EClass.class).get();
+						source.getESuperTypes().remove(target);
+					});
+				}
+			});
 		});
+
 		modelState.getIndex().getSemantic(elementId).ifPresent(EcoreUtil::remove);
 		modelState.getIndex().getNotation(elementId).ifPresent(EcoreUtil::remove);
 		return true;
 	}
-	
+
 }
